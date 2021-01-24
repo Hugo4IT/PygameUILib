f4d308ae3bea8a15bb0d229cb44e09fc9a73a4e7
Hugo van de Kuilen from Hugo4IT.com <hugo.vandekuilen1234567890@gmail.com>
2021-01-24 17:57:55 +0100

    PLS WORK

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PygameUILib",
-    version="0.0.1",
+    version="0.0.2",
     author="Hugo van de Kuilen from Hugo4IT.com",
     author_email="hugo.vandekuilen1234567890@gmail.com",
     description="An easy-to-use pygame User Interface Library",
